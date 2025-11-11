e68aacffd7efdc5d77f423e746e056a119c5fc8e
andrewgravel <andrew.gravel@csuglobal.edu>
2025-11-11 16:49:04 -0700

    Add files via upload

---
+++ PersonInfo.java
@@ -0,0 +1,25 @@
+package personinfo;
+
+public class PersonInfo {
+
+	    public static void main(String[] args) {
+	        String firstName = "John";
+	        String lastName = "Doe";
+	        String streetAddress = "123 Deer Drive";
+	        String city = "Denver";
+	        String zipCode = "80022";
+
+	        // Each println prints on its own line
+	        System.out.println(firstName);
+	        System.out.println(lastName);
+	        System.out.println(streetAddress);
+	        System.out.println(city);
+	        System.out.println(zipCode);
+	    }
+	}
+
+
+
+	
+
+
